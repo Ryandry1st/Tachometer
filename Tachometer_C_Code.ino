5595f7ea6e4f6f140b803618fe905c8b5f59a6bf
Ryan Dreifuerst <dreifuerstrm@msoe.edu>
2017-08-07 22:30:10 -0500

    Update

--- Tachometer_C_Code.ino
+++ Tachometer_C_Code.ino
@@ -5,9 +5,9 @@
 //max time between 1/10 of the wheel is 4.2s
 //time between counts is 64us
 //Must have a high logic level defined by 2.6V or greater
-#define ReadsPerRotation 1
-#define Average 2
-#define Send 10
+#define readsPerRotation 1
+#define Average 1
+#define Send 15
 
 
 void Timerreset(){
@@ -91,10 +91,10 @@ void loop() {
 
   Timerreset();
   while(PINH & 1<<PH3 && error == 1){
-    delay(5);
+    //delay(5);
     if(!(PINA & 1<<PA0)){
       while(!(PINA & 1<<PA0));
-      Serial.println(TCNT1, DEC);
+      //Serial.println(TCNT1, DEC);
       if(indexA == Average){
         if(indexS == Send){
           Serial.println("max data stored, stopping");
@@ -116,8 +116,9 @@ void loop() {
       else{
         toAverage[indexA] = TCNT1;
         totaltime += toAverage[indexA];
-        Timerreset();
         indexA++;
+        PORTC ^= 1<<PC7;
+        Timerreset();
       }
     }
     else if(TIFR1 & 1){
@@ -129,9 +130,11 @@ void loop() {
   PORTA &=~1<<PA1;
   PORTC |= 1<<PC7;
   for(int j = 0; j< Send; j++){
+    toSend[j] = readsPerRotation/(toSend[j]*64.0/1000000.0); //in rotations per seconds
     Serial.println(toSend[j]);
   }
   Serial.println("done");
-  Serial.println(totaltime);
+ 
+  Serial.println(totaltime*64/1000000.0);
   delay(10000);
 }
